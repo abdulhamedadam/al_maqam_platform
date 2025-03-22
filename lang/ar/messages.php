31b86279506a7be753b4f4caa28a0fc4cce197a9
abdulhamedadam <abdulhamedzaghloul@gmail.com>
2025-03-22 13:43:24 +0200

    s

--- lang/ar/messages.php
+++ lang/ar/messages.php
@@ -4,4 +4,8 @@
   'Wait For Admin Approval' => 'Wait For Admin Approval',
   'تم قبول المعلم بنجاح' => 'تم قبول المعلم بنجاح',
   'Registration successful!' => 'Registration successful!',
+  'تم إضافة الخدمة بنجاح' => 'تم إضافة الخدمة بنجاح',
+  'تم حذف الخدمة بنجاح' => 'تم حذف الخدمة بنجاح',
+  'Login successful!' => 'Login successful!',
+  'Logout successful!' => 'Logout successful!',
 );
